18245aa12b0b6d3f1599644226767c441aea1ad6
isharaut1720 <ishaaraut65@gmail.com>
2026-04-30 21:51:34 +0530

    Updated API URL for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://ai-resume-analyzer-wygn.onrender.com",
   headers: {
     "Content-Type": "multipart/form-data",
   },
